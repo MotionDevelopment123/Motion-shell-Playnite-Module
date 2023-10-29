3cf8812d2be6cbbe1940abc485b6fe73bc0d597e
MotionDevelopment123 <126352749+MotionDevelopment123@users.noreply.github.com>
2023-10-29 22:57:05 +0000

    Delete Splash/Scripts/Playnite-Shutdown-Script.ps1

--- Splash/Scripts/Playnite-Shutdown-Script.ps1
+++
@@ -1,8 +0,0 @@
-if ($PlayniteApi.ApplicationInfo.Mode -eq "Fullscreen")
-{
-START-PROCESS "C:\Playnite\Playnite-Shell\EXE\Windows-Shell.EXE"
-}
-else
-{
-    # Execute when running in Fullscreen mode
-}
