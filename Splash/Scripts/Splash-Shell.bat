dfe590e907b6494fe014ce8ccc94202c58ef24e7
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-10-29 18:45:43 +0000

    Adding scripts

---
+++ Splash/Scripts/Splash-Shell.bat
@@ -0,0 +1,2 @@
+start "" "C:\playnite\Playnite.fullscreenapp.exe" --hidesplashscreen
+start "" "C:\ffmpeg-6.0-essentials_build\bin\ffplay.exe" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "C:\Videos\splash.mp4" 2>NUL
