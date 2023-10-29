185060f821e6d5d8ed58fba07ff343cba041a655
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-10-29 23:32:37 +0000

    Update and moved items

--- Splash/Scripts/Splash-Shell.bat
+++ Splash/Scripts/Splash-Shell.bat
@@ -1,2 +1,5 @@
-start "" "C:\playnite\Playnite.fullscreenapp.exe" --hidesplashscreen
-start "" "C:\ffmpeg-6.0-essentials_build\bin\ffplay.exe" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "C:\Videos\splash.mp4" 2>NUL
+CD C:\Playnite-Shell
+set /p PLAYNITELOCATION=<Playnite.LOCATION
+set /p FFPLAY=<FFPLAY.LOCATION
+start "" "%PLAYNITELOCATION%\Playnite.fullscreenapp.exe" --hidesplashscreen
+start "" "%FFPLAY%" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "C:\Videos\splash.mp4" 2>NUL
