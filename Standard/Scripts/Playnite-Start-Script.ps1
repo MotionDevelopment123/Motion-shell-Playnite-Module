cff676cfc5321eb45191830556feef1f0cd05388
MotionDevelopment123 <126352749+MotionDevelopment123@users.noreply.github.com>
2023-10-29 22:54:02 +0000

    Update Playnite-Start-Script.ps1

--- Standard/Scripts/Playnite-Start-Script.ps1
+++ Standard/Scripts/Playnite-Start-Script.ps1
@@ -8,7 +8,6 @@ else{
         if ($PlayniteApi.ApplicationInfo.Mode -eq "Fullscreen")
         {
         TASKKILL /IM EXPLORER.EXE /F
-        START-PROCESS "G:\SteamLibrary\steamapps\common\DSX\Openmin.bat"
         }
         else
         {
