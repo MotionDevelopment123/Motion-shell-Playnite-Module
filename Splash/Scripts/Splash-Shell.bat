bd4eb010704560315e769d6f1f5d12e1f46e1a8c
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-10-30 22:30:51 +0000

    Added variables

--- Splash/Scripts/Splash-Shell.bat
+++ Splash/Scripts/Splash-Shell.bat
@@ -1,5 +1,7 @@
 CD C:\Playnite-Shell
-set /p PLAYNITELOCATION=<Playnite.LOCATION
+set /p SIZE=<SCREENSIZE.PIXELS
+set /p VIDEO=<VIDEO.LOCATION
+set /p PLAYNITE=<Playnite.LOCATION
 set /p FFPLAY=<FFPLAY.LOCATION
-start "" "%PLAYNITELOCATION%\Playnite.fullscreenapp.exe" --hidesplashscreen
-start "" "%FFPLAY%" -left 0 -top 0 -x 1920 -y 1080 -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "C:\Videos\splash.mp4" 2>NUL
+start "" "%PLAYNITELOCATION%" --hidesplashscreen
+start "" "%FFPLAY%" -left 0 -top 0 %SIZE% -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "%VIDEO%" 2>NUL
