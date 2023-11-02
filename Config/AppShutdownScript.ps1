de36e4679d4fa07617518b6777059f3bd2152910
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-11-02 23:22:22 +0000

    Fixed config-Script

--- Config/AppShutdownScript.ps1
+++
@@ -1,5 +0,0 @@
-$playniteDirectory = "C:\Games\Playnite"
-$configPath = [System.IO.Path]::Combine($playniteDirectory, "config.json")
-$config = [System.IO.File]::ReadAllLines($configPath) | ConvertFrom-Json
-$config.AppShutdownScript = "if ($PlayniteApi.ApplicationInfo.Mode -eq \"Fullscreen\")\r\n{\r\nSTART-PROCESS \"C:\\SHELL\\SHELL.EXE\"\r\n}\r\nelse\r\n{\r\n    # Execute when running in Fullscreen mode\r\n}"
-$config | ConvertTo-Json -Depth 10 | Out-File $configPath -Encoding utf8
