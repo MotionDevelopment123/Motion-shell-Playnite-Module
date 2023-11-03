f750ff5048af6f6f750f6535507963b4d5bb07a6
MotionDevelopment123 <unknown.motion123@gmail.com>
2023-11-03 21:27:21 +0000

    Update

--- Splash-Shell.bat
+++ Splash-Shell.bat
@@ -3,5 +3,5 @@ set /p SIZE=<SCREENSIZE.PIXELS
 set /p VIDEO=<VIDEO.LOCATION
 set /p PLAYNITE=<Playnite.LOCATION
 set /p FFPLAY=<FFPLAY.LOCATION
-start "" "%PLAYNITELOCATION%" --hidesplashscreen
+start "" "%PLAYNITE%Playnite.FullscreenApp.exe" --hidesplashscreen
 start "" "%FFPLAY%" -left 0 -top 0 %SIZE% -alwaysontop -noborder -autoexit -loglevel quiet -loop 1 "%VIDEO%" 2>NUL
